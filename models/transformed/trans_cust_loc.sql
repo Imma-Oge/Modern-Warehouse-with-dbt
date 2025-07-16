632537cff03c13eccc2e1b565a0544f589fddf90
David Shegun <dshegs01@gmail.com>
2025-07-16 08:09:35 +0000

    transformed the customer tables

---
+++ models/transformed/trans_cust_loc.sql
@@ -0,0 +1,17 @@
+with
+source as (
+    select * from {{ref ('stg_cust_loc')}}
+),
+trans_cust_loc as (
+    select
+        replace (cid, '-', '') as cid,
+        case
+            when cntry in ('US', 'United States', 'USA') then 'United States'
+            when cntry in ('Germany', 'DE') then 'Germany'
+            when cntry in ('Australia', 'Canada', 'France', 'United Kingdom') then cntry
+            else 'n\a'
+        end as cntry
+    from source
+)
+
+select * from trans_cust_loc

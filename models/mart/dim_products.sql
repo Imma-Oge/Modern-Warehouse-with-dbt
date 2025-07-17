c8ac7e815f5977b042834144d58856911fb7e124
David Shegun <111058952+Davshegs@users.noreply.github.com>
2025-07-17 20:21:46 +0100

    Revert "."

--- models/mart/dim_products.sql
+++
@@ -1,5 +0,0 @@
-with mart_dim_products as
-(
-    select
-        {{dbt_utils.generate_surrogate_key(['cst_id', 'cst_key'])}} as customer_key,
-)
--- tests/test_category_id.sql
+++
@@ -1,8 +0,0 @@
-with prd_test as
-  (
-     select category_id 
-from {{ref("trans_prd_info")}}
-  ) 
-  select category_id
-  from {{ref("trans_prd_cat")}}
-where category_id not in (select * from prd_test) 

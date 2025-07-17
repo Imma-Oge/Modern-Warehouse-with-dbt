8f8528ec98da59792a09d74fc61c13681b4115ad
Immaculate Okoro <ermmaculate97@gmail.com>
2025-07-17 21:31:12 +0000

    commit

--- models/mart/mart_dim_products.sql
+++ models/mart/mart_dim_products.sql
@@ -10,10 +10,10 @@ with mart_dim_products as
         sub_category,
         maintainance,
         production_line,
-        start_date,
-         end_date
+        start_date
     from {{ref("trans_prd_info")}} p
     left join {{ref("trans_prd_cat")}} c 
     on p.category_id = c.id
+    where end_date is null
 )
 select * from mart_dim_products
